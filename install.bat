c0772c1cc665750d079b5b2f804dae7e6c6acf0a
privatefitout-design <privatefitout@gmail.com>
2026-04-16 15:02:50 +0400

    Delete install.bat

--- install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Устанавливаем зависимости для Scanner 1...
-pip install requests pandas apscheduler python-telegram-bot python-dotenv
-echo.
-echo Готово! Теперь запусти: python scanner1604.py
-pause
