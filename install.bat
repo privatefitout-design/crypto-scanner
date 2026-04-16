03b71a30f0a8c476e122bdbe2eddfc4d7d8e2c04
privatefitout-design <privatefitout@gmail.com>
2026-04-16 14:23:49 +0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@
 echo Устанавливаем зависимости для Scanner 1...
 pip install requests pandas apscheduler python-telegram-bot python-dotenv
 echo.
-echo Готово! Теперь запусти: python scanner1_accumulation.py
+echo Готово! Теперь запусти: python scanner1604.py
 pause
